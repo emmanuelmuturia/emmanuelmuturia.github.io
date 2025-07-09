3768d7bd149d1a9c20da91f0840b06ab4c671efc
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-07-09 15:48:15 +0300

    Refine the Professional Experience Carousel...

--- src/portfolio.js
+++ src/portfolio.js
@@ -140,20 +140,9 @@ const workExperiences = {
       ]
     },
     {
-      role: "Android Engineering",
-      company: "DroidCon Kenya",
-      companylogo: require("./assets/images/droidcon_kenya.png"),
-      date: "June 2023 – Present",
-      link: "https://www.linkedin.com/in/emmanuelmuturia/", // Add link attribute
-      descBullets: [
-        "Resolved a critical issue on the “Filter” button in the “Sessions” tab, enhancing Jetpack Compose UI stability and reducing app crashes leading to the release of the final product with over 100 downloads",
-        "Optimised Android SDK components by eliminating redundant String declarations and improving CI/CD processes"
-      ]
-    },
-    {
       role: "Android Development [Contract]",
       company: "CIFOR-ICRAF",
-      companylogo: require("./assets/images/cifor_icraf.jpeg"),
+      companylogo: require("./assets/images/cifor_icraf.png"),
       date: "June 2024 – Sep 2024",
       link: "https://www.linkedin.com/in/emmanuelmuturia/", // Add link attribute
       descBullets: [
@@ -164,7 +153,7 @@ const workExperiences = {
     {
       role: "Network Operations Centre Engineering [Internship]",
       company: "Kinde Engineering Works Ltd.",
-      companylogo: require("./assets/images/kinde_engineering_works.png"),
+      companylogo: require("./assets/images/kinde_engineering_works_ltd.png"),
       date: "Feb 2023 – April 2023",
       link: "https://www.linkedin.com/in/emmanuelmuturia/", // Add link attribute
       descBullets: [
