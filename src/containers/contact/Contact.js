d3ee825c3c0a17ec62f33437b0c6318a03d52686
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-07-14 15:33:32 +0300

    Remove the useless Images and refine the Favicons...

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -41,12 +41,7 @@ export default function Contact() {
           <div className="contact-image-div">
             {illustration.animated ? (
               <DisplayLottie animationData={email} />
-            ) : (
-              <img
-                alt="Man working"
-                src={require("../../assets/images/contactMailDark.svg")}
-              ></img>
-            )}
+            ) : null}
           </div>
         </div>
       </div>
