ba61c49df76114f5e1da6cce4618c59bcf7ac24f
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-07-14 15:24:28 +0300

    Implement the new logo...

--- src/containers/splashScreen/SplashScreen.js
+++ src/containers/splashScreen/SplashScreen.js
@@ -1,19 +1,11 @@
 import React from "react";
 import "./SplashScreen.css";
-import DisplayLottie from "../../components/displayLottie/DisplayLottie";
-import {greeting, splashScreen} from "../../portfolio";
+import logo from "../../assets/images/logo.png";
 
 export default function SplashScreen() {
   return (
     <div className="splash-container">
-      <div className="splash-animation-container">
-        <DisplayLottie animationData={splashScreen.animation} />
-      </div>
-      <div className="splash-title-container">
-        <span className="grey-color"> &lt;</span>
-        <span className="splash-title">{greeting.username}</span>
-        <span className="grey-color">/&gt;</span>
-      </div>
+      <img src={logo} alt="Logo" className="splash-logo" />
     </div>
   );
 }
