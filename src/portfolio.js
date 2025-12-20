8506e820942a1ff4b93b9c7ba0535db2cc626e51
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-12-20 17:59:09 +0300

    Remove The Education Section...

--- src/portfolio.js
+++ src/portfolio.js
@@ -88,7 +88,7 @@ https://fontawesome.com/icons?d=gallery */
 // Education Section
 
 const educationInfo = {
-  display: true, // Set false to hide this section, defaults to true
+  display: false, // Set false to hide this section, defaults to true
   schools: [
     {
       schoolName: "Strathmore University",
